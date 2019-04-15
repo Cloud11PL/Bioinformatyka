f9d5e3c9760eab72dd3aaa9ae73f1c995f8b868f
Wojciech Mista <wojciech.mista@hotmail.com>
2019-04-15 23:56:43 +0200

    kinda working path finding algorithm

--- testing.m
+++ testing.m
@@ -11,8 +11,8 @@ seq2 = ['-','A','T','A'];
 scoredMatrix = smithWaterman(-2,seq1,seq2)
 
 %find max value
-[x,y] = max(scoredMatrix)
-maxValue = max(max(scoredMatrix))
+[x,y] = max(scoredMatrix);
+maxValue = max(max(scoredMatrix));
 
 XCorLength = length(scoredMatrix(:,1));
 YCorLength = length(scoredMatrix(1,:));
@@ -24,44 +24,50 @@ YCor = [];
 for m = 1:XCorLength
     for n = 1:YCorLength
         if scoredMatrix(m,n) == maxValue
-            XCor(end+1) = m
-            YCor(end+1) = n
+            XCor(end+1) = m;
+            YCor(end+1) = n;
         end
     end
 end
 
 %find a path
+%localMax = maxValue;
 
-curRow = XCor(1)
-curCol = YCor(1)
+% curRow = XCor(1);
+% curCol = YCor(1);
 matrixPath = zeros(XCorLength,YCorLength);
-
-while curRow ~= 0 || curCol ~= 0
-    matrixPath(curRow, curCol) = 1
-    %w lewo
-    left = scoredMatrix(curRow,curCol - 1);
-    %w gore
-    up = scoredMatrix(curRow - 1, curCol);
-    %na skosss
-    diag = scoredMatrix(curRow - 1, curCol - 1);
-    
-    [localMax,pos] = max([left up diag])
-    
-    if(localMax > 0)
-        if(pos == 1)
-            curCol = curCol - 1
-        elseif(pos == 2)
-            curRow = curRow - 1
-        else
-            curRow = curRow - 1
-            curCol = curCol - 1
+index = 1;
+while index <= length(XCor)
+    curRow = XCor(index);
+    curCol = YCor(index);
+    index = index + 1;
+    localMax = maxValue;
+    while localMax ~= 0
+        matrixPath(curRow, curCol) = 1;
+        %w lewo
+        left = scoredMatrix(curRow,curCol - 1);
+        %w gore
+        up = scoredMatrix(curRow - 1, curCol);
+        %na skosss
+        diag = scoredMatrix(curRow - 1, curCol - 1);
+        
+        [localMax,pos] = max([left up diag]);
+        
+        if(localMax > 0)
+            if(pos == 1)
+                curCol = curCol - 1;
+            elseif(pos == 2)
+                curRow = curRow - 1;
+            else
+                curRow = curRow - 1;
+                curCol = curCol - 1;
+            end
+            %     else
+            %         matrixPath(curRow - 1, curCol - 1) = 1;
         end
-    else
-        matrixPath(curRow - 1, curCol - 1) = 1
     end
 end
 
-%%heatmap(matrixPath)
-matrixPath
+heatmap(matrixPath)
 
             
