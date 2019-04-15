cd96e973ad2a02afda4837fdc4663b72a72476a6
Wojciech Mista <wojciech.mista@hotmail.com>
2019-04-15 21:07:11 +0200

    almost working smith-waterman

--- testing.m
+++ testing.m
@@ -1,4 +1,10 @@
-seq1 = ['A','C','T','G'];
-seq2 = ['A','C','C','G'];
+clc
+clear
+%AATCG
+%seq1 = ['-','A','C','T','G'];%minus ulatwia dalsze obliczenia
+%seq2 = ['-','A','C','C','G'];
 
-xDDD = smithWaterman(-4,seq1,seq2)
+seq1 = ['-','A','A','T','C','G'];%minus ulatwia dalsze obliczenia
+seq2 = ['-','A','A','C','G'];
+
+xDDD = smithWaterman(-2,seq1,seq2)
